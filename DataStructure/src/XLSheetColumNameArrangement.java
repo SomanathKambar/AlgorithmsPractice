542900584260bcbe0f868003e4bc0dd687763712
SomanathTotality <124239783+SomanathTotality@users.noreply.github.com>
2024-03-07 12:12:21 +0530

    Adding XLSheetColumNameArrangement Implementation

---
+++ DataStructure/src/XLSheetColumNameArrangement.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class XLSheetColumNameArrangement {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        int counter =0 ;
+        for(char ch= 'A'; ch <= 'Z'; ++ ch) {
+            map.put(String.valueOf(ch),++counter);
+        }
+
+        System.out.println(map);
+
+        String input = "ZY";
+        System.out.println(getFaceValue(input, map));
+
+    }
+
+    public static int getFaceValue(String value,Map<String, Integer> map) {
+        String[] input = value.split("");
+        int result = 0;
+        for(int i=0; i<input.length;i++){
+            if(map.containsKey(input[i])) {
+                if(i == 0) {
+                    result = map.get(input[i]);
+                    result = (result * 26);
+                } else {
+                    // res =0, 26+ (1*1) = 26+1 = 27
+                    result = result + (map.get(input[i]));
+                }
+            }
+        }
+        return  result;
+    }
+
+}
+
